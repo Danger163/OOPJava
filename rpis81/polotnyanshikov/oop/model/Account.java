b2a5aad907d31746770eeb4b51b2f446bc77b493
rusnab99 <rusnab99@gmail.com>
2020-03-24 19:43:30 +0400

    Lab1 completed

---
+++ rpis81/polotnyanshikov/oop/model/Account.java
@@ -0,0 +1,37 @@
+package rpis81.polotnyanshikov.oop.model;
+
+public class Account {
+    String number;
+    double balance;
+
+    public String getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(String number)
+    {
+        this.number=number;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void setBalance(double balance)
+    {
+        this.balance=balance;
+    }
+
+    public Account()
+    {
+        number="";
+        balance=0.0;
+    }
+
+    public Account(String number, double balance)
+    {
+        this.number=number;
+        this.balance=balance;
+    }
+}
